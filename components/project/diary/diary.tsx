d54e63476d2b82ef850528ae7d187496651d3112
春秋半夏 <yin_chunyang@163.com>
2025-05-20 18:36:13 +0800

    feat: about temp content

--- components/project/diary/diary.tsx
+++ components/project/diary/diary.tsx
@@ -27,15 +27,15 @@ export const Diary = ({
             className={cn('absolute w-full h-full left-[-8px] top-[8px] bg-background z-[-1]', 'shadow-[0_0_10px_rgba(0,0,0,0.2)] dark:shadow-[0_0_8px_rgba(255,255,255,.1)]', style.background, className2)}></div>
         <div className={cn('w-full flex justify-between items-center pb-[10px] relative px-[10px]')}>
             <div className='absolute w-full h-[3px] bg-[#0AA9DB] left-0 bottom-0'></div>
-            <div className={cn('text-[18px]')}>
+            <div className={cn('text-[18px] cursor-default')}>
                 {title}
             </div>
-            <div className={cn('flex text-[14px] items-center')}>
+            <div className={cn('flex text-[14px] items-center cursor-default')}>
                 <span className='mr-[2px] h-[14px] flex items-center'>{date}</span>
                 {weather}
             </div>
         </div>
-        <div className={cn(style.content, className3)}>
+        <div className={cn(style.content, className3,'cursor-default')}>
             {content}
         </div>
     </div>
